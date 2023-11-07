0ccf3b23d5c3a8e848e0f20fa49688a8c276e04e
BrnCalisario <bruno145224@gmail.com>
2023-11-07 10:21:31 -0300

    reorganizing pt2

--- src/fight/Program.cs
+++ src/fight/Program.cs
@@ -1,22 +1,43 @@
-﻿// const string m1 = "m1.txt";
-// const string m2 = "m2.txt";
+﻿const string m1 = "m1.txt";
+const string m2 = "m2.txt";
 
-// while(true)
-// {
-    
-// }
+TicTacToe game = TicTacToe.New();
+
+while(true)
+{
+    Thread.Sleep(500);
+
+    try {
+        if(game.PlayerOneTurn)
+            get(m1, m2);
+        else 
+            get(m2, m1);
+    } catch {}
+
+    if(game.GameOver)
+        break;
+}
+
+string winner = game.Winner == 0 ? "( X )" : "( O )";
+System.Console.WriteLine("Game Over, winner : " + winner);
 
 
+void get(string path, string other)
+{
+    if(!File.Exists(path))
+        return;
 
-// // void get(string path, string other)
-// // {
-// //     if(!File.Exists(path))
-// //         return;
+    var text = File.ReadAllText(path);
 
-// //     var text = File.ReadAllText(path);
 
-// //     if(!byte.TryParse(text, out pos))
-// //     {
+    if (!byte.TryParse(text, out byte pos))
+        return;
 
-// //     }
-// // }'
+    if (!game.TryPlay(pos))
+        return;
+
+    File.Delete(path);
+    File.WriteAllText("[OUTPUT]" + other, pos.ToString());
+
+    System.Console.WriteLine(game);
+}
--- src/tic-tac-toe/TicTacToe.cs
+++ src/tic-tac-toe/TicTacToe.cs
@@ -1,6 +1,5 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Text;
 
 public struct TicTacToe
